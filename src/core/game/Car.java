24b66a3fd8c0917901a23826f324eddd97f49be0
MattBarkerUNI <mattbarkeruni@gmail.com>
2019-11-29 14:27:40 +0000

    in the process of adding velocity and max speed to the car

--- src/core/game/Car.java
+++ src/core/game/Car.java
@@ -1,13 +1,16 @@
 package core.game;
 import core.game_engine.Sprite;
 import core.game_engine.input_commands.MoveAble;
+import core.game_engine.physics.PhysicsComponent;
 import processing.core.PApplet;
 import processing.core.PVector;
 import core.game_engine.GameObject;
 
 public class Car extends Sprite implements MoveAble {
     public PVector size;
-    float angle = (float) 0.06981317007;
+    private PhysicsComponent physicsComponent;
+    public float acceleration = 2f;
+    float angle = (float) 0.08;
     float rotation = 0;
     PVector move = new PVector(250, 250, 0);
     PVector dir = new PVector(0, 1, 0);
@@ -17,12 +20,13 @@ public class Car extends Sprite implements MoveAble {
         super(p, x, y, w, h);
         this.parent = p;
         this.size = new PVector(w, h, 0);
+        physicsComponent = new PhysicsComponent(this); // reference to this current gameObject being added in
     }
 
     @Override
     public void update() {
+        super.update();
         this.parent.pushMatrix();
-        //this.parent.rectMode(CENTER);
         this.parent.translate(move.x, move.y);
         this.parent.rotate(rotation);
         this.parent.rect(0, 0, 30, 50);
@@ -37,24 +41,25 @@ public class Car extends Sprite implements MoveAble {
         if (fwd) {
             move.add(dir.setMag(5));
         }
-        //move(); //performs movement controls to allow the car to move
-
     }
 
     @Override
     public void moveLeft() {
         rotation -= angle;
         dir.rotate(-angle);
+        //this.physicsComponent.setVelocity(-acceleration, 0);
     }
 
     @Override
     public void moveRight() {
         rotation += angle;
         dir.rotate(angle);
+        //this.physicsComponent.setVelocity(acceleration, 0);
     }
 
     @Override
     public void moveForward() {
         move.add(dir.setMag(5));
+        //this.physicsComponent.setVelocity(0, acceleration);
     }
 }
--- src/core/game_engine/physics/PhysicsComponent.java
+++ src/core/game_engine/physics/PhysicsComponent.java
@@ -2,8 +2,11 @@ package core.game_engine.physics;
 
 import core.game_engine.Component;
 import core.game_engine.Sprite;
+import processing.core.PVector;
 
 public class PhysicsComponent extends Component {
+    private PVector velocity = new PVector(0, 0, 0);
+    public float maxSpeed = 5f;
     public PhysicsComponent(Sprite g){
         super(g);
 
@@ -11,6 +14,14 @@ public class PhysicsComponent extends Component {
 
     @Override
     public void update() {
+        if(velocity.mag() > maxSpeed){
+            velocity.setMag(maxSpeed);
+        }
+        this.gameObject.position.add(velocity);
+    }
 
+    public void setVelocity(float x, float y) {
+        velocity.x += x;
+        velocity.y += y;
     }
 }
